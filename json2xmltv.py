e4883deaa71c4860ad74fcf61f277665c65904d0
Jir14 <jirkakozarek@seznam.cz>
2026-07-18 11:34:21 +0200

    special characters fixed

--- json2xmltv.py
+++ json2xmltv.py
@@ -4,6 +4,14 @@ from datetime import datetime, timedelta
 r = requests.get('https://api.instant.audio/data/playlist/76/energy', headers={'User-Agent': 'Foo bar'})
 fileName="radioenergy.xml"
 
+def specChar(str):
+    str=str.replace('"', '&quot;')
+    str=str.replace("'", '&apos;')
+    str=str.replace('<', '&lt;')
+    str=str.replace('>', '&gt;')
+    str=str.replace('&', '&amp;')
+    return str
+
 json = r.json()
 if json["success"]:
     with open(fileName, "w") as f:
@@ -16,8 +24,10 @@ if json["success"]:
         time2=time+timedelta(minutes=3)
         with open(fileName, "a") as f:
             f.write('<programme start="'+time.strftime('%Y%m%d%H%M%S')+' +0000" stop="'+time2.strftime('%Y%m%d%H%M%S')+' +0000" channel="radioenergy">\n')
-            f.write('<title>'+r["track_title"]+'</title>\n')
-            f.write('<sub-title>\n'+r["track_artist"]+'\n</sub-title>\n')
-            f.write('<image type="poster">'+r["track_image"]+'</image>\n')
+            f.write('<title>'+specChar(r["track_title"])+'</title>\n')
+            f.write('<sub-title>\n'+specChar(r["track_artist"])+'\n</sub-title>\n')
+            f.write('<image type="poster">'+specChar(r["track_image"])+'</image>\n')
             f.write('</programme>\n')
         time=time2
+    with open(fileName, "a") as f:
+        f.write('</tv>')
